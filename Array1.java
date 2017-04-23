fabbe41f459c7365809a4e3a285f714900aef3b0
NguyenXuanAnhThu <thutk08@gmail.com>
2017-04-23 13:35:47 +0700

    Add files via upload

---
+++ Array1.java
@@ -0,0 +1,11 @@
+// Tính tổng các phần tử của mảng.
+package array1;
+public class Array1 {
+    public static void main(String[] args) {
+        int[] a1 = {1,5,2,4};
+        int[] a2 = {-2,-5,-6,-7};
+        int[] a3 = {-7,3,-8,9};
+    
+    }
+        
+ }
